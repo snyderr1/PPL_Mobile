fe8831dfb59a4b7edeae7a4c7077e01ad9e991eb
Ross Snyder <rossesny@gmail.com>
2024-01-05 05:47:42 -0800

    refactor recyclerview- add floating add item button and corresponding class  in adapter

--- app/src/main/java/com/example/pplmobile/ExerciseAdapter.kt
+++ app/src/main/java/com/example/pplmobile/ExerciseAdapter.kt
@@ -11,45 +11,66 @@ import com.example.pplmobile.database.Exercise
 import com.example.pplmobile.database.ExerciseRoomDatabase
 import kotlinx.coroutines.CoroutineScope
 
-class ExerciseAdapter(private val exerciseList: List<Exercise>, private val currentViewModel: ExerciseViewModel): RecyclerView.Adapter<ExerciseAdapter.ViewHolder>()  {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recycler_view_item, parent, false)
+class ExerciseAdapter(private val exerciseList: List<Exercise>, private val currentViewModel: ExerciseViewModel): RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
+    private val CONTENT = 1
+    private val ADD = 2
 
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType == CONTENT) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_view_item, parent, false)
+            return ContentViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_add_item, parent, false)
+            return AddButtonViewHolder(view)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int){
-        val exerciseDataInstance = exerciseList[position]
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder.itemViewType == CONTENT) {
+            holder as ContentViewHolder
+            val exerciseDataInstance = exerciseList[position]
 
-        //holder.exerciseIcon.setImageResource(exerciseDataInstance.image)
+            //holder.exerciseIcon.setImageResource(exerciseDataInstance.image)
 
-        holder.exerciseName.text = exerciseDataInstance.exerciseName
-        holder.exerciseQuantity.text = exerciseDataInstance.exerciseQuantity
-        holder.editButton.setOnClickListener{
-            if(holder.exerciseName.isEnabled) {
-                val newExercise: Exercise = Exercise(
-                    exerciseName=holder.exerciseName.getText().toString(),
-                    exerciseQuantity=holder.exerciseQuantity.getText().toString(),
-                    exerciseType="placeholder")
-                currentViewModel.insert(newExercise)
+            holder.exerciseName.text = exerciseDataInstance.exerciseName
+            holder.exerciseQuantity.text = exerciseDataInstance.exerciseQuantity
+            holder.editButton.setOnClickListener {
+                if (holder.exerciseName.isEnabled) {
+                    val newExercise: Exercise = Exercise(
+                        exerciseName = holder.exerciseName.getText().toString(),
+                        exerciseQuantity = holder.exerciseQuantity.getText().toString(),
+                        exerciseType = "placeholder"
+                    )
+                    currentViewModel.insert(newExercise)
+                }
+                holder.exerciseName.isEnabled = !(holder.exerciseQuantity.isEnabled)
+                holder.exerciseQuantity.isEnabled = !(holder.exerciseQuantity.isEnabled)
+
             }
-            holder.exerciseName.isEnabled = !(holder.exerciseQuantity.isEnabled)
-            holder.exerciseQuantity.isEnabled = !(holder.exerciseQuantity.isEnabled)
+        }
 
+    }
+
+    override fun getItemViewType(position:Int): Int{
+        if(position == exerciseList.size){
+            return ADD
+        } else {
+            return CONTENT
         }
     }
 
     override fun getItemCount(): Int {
-        return exerciseList.size
+        return exerciseList.size + 1
     }
 
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    class ContentViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val exerciseIcon: ImageView = itemView.findViewById(R.id.exerciseIcon)
         val exerciseName: TextView = itemView.findViewById(R.id.exerciseName)
         val exerciseQuantity: TextView = itemView.findViewById(R.id.exerciseQuantity)
         val editButton: android.widget.Button = itemView.findViewById(R.id.editButton)
+    }
+    class AddButtonViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
     }
 }
